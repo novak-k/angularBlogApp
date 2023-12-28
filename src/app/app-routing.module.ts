ba03c5d13ee97522456aa02822ac2d9b9c12161e
Kate <novak5683@gmail.com>
2023-12-28 21:25:18 +0100

    admin page bugfix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,8 @@ const routes: Routes = [
   },
   {
     path: 'admin', loadChildren: () => import('./admin/admin.module').then(x => x.AdminModule)
-  }
+  },
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
